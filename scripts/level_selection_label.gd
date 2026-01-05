21f57164afb5c7523ddbd8b5c5fe70fb8b4a7dcd
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-01-05 20:41:38 +0100

    Added fallback to level selection label config

--- scripts/level_selection_label.gd
+++ scripts/level_selection_label.gd
@@ -1,4 +1,4 @@
 extends ReferenceRect
 class_name LevelSelectionLabelRect
 
-@export var corresponding_level: SceneManager.LevelIds
+@export var corresponding_level: SceneManager.LevelIds = SceneManager.LevelIds.LEVEL1
