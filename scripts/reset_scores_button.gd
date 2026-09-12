fe22a352490f73a1a450f62110a0f73c9aec19b7
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-09-13 01:39:26 +0200

    Require confirmation before resetting savegame

--- scripts/reset_scores_button.gd
+++ scripts/reset_scores_button.gd
@@ -1,5 +1,34 @@
 extends ButtonPreset
 
 
+@export_multiline var danger_text: String = ""
+@export var danger_modulate: Color
+
+@export var label: Label
+@export var hide_on_danger: CanvasItem
+
+var danger_active: bool = false
+var base_text: String
+
+func _ready() -> void:
+	super._ready()
+	base_text = label.text
+
+func unselect() -> void:
+	no_danger()
+
 func press() -> void:
-	ManagerGlobal.reset_game()
+	if !danger_active:
+		danger_active = true
+		label.text = danger_text
+		hide_on_danger.hide()
+		modulate = danger_modulate
+	elif danger_active:
+		ManagerGlobal.reset_game()
+		modulate = selected_modulate
+		no_danger()
+
+func no_danger() -> void:
+	danger_active = false
+	label.text = base_text
+	hide_on_danger.show()
