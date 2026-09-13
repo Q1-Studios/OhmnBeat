3131a4975a8ea4847f522166953e4bbb74fca26c
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-09-13 04:37:22 +0200

    Greatly improve behaviour of mobile buttons

--- scripts/button_grace_area_mobile.gd
+++ scripts/button_grace_area_mobile.gd
@@ -5,6 +5,7 @@ class_name ButtonGraceAreaMobile
 
 func _ready() -> void:
 	super._ready()
+	proxy_for = actual_button
 	if not Globals.is_mobile:
 		hide()
 
@@ -12,13 +13,5 @@ func _process(_delta: float) -> void:
 	selectable = actual_button.selectable
 	disabled = actual_button.disabled
 
-func select() -> void:
-	actual_button.grab_focus()
-
-func unselect() -> void:
-	if not actual_button.mouse_inside:
-		actual_button.release_focus()
-
 func press() -> void:
-		actual_button.grab_focus()
-		actual_button._on_pressed()
+		actual_button.pressed.emit()
--- scripts/button_preset.gd
+++ scripts/button_preset.gd
@@ -5,6 +5,8 @@ class_name ButtonPreset
 
 @onready var default_modulate: Color = modulate
 
+var proxy_for: ButtonPreset = self
+
 var selected: bool = false
 @export var selectable: bool = true
 
@@ -20,14 +22,16 @@ func _ready() -> void:
 func _on_mouse_entered() -> void:
 	if is_visible_in_tree() and selectable:
 		mouse_inside = true
-		Vibration.gui_tap()
-		grab_focus()
-	
+		proxy_grab_focus()
+
 func _on_mouse_exited() -> void:
 	mouse_inside = false
-	release_focus()
+	proxy_release_focus()
 
 func _on_focus_entered() -> void:
+	if(proxy_for != self):
+		proxy_for.grab_focus()
+		return
 	if selectable:
 		modulate = selected_modulate
 		select()
@@ -42,6 +46,15 @@ func _on_pressed() -> void:
 func set_selectable(value: bool) -> void:
 	selectable = value
 	_on_mouse_exited()
+
+func proxy_grab_focus() -> void:
+	if(not proxy_for.has_focus()):
+		Vibration.gui_tap()
+		proxy_for.grab_focus()
+
+func proxy_release_focus() -> void:
+	if(not proxy_for.mouse_inside):
+		proxy_for.release_focus()
 
 # Inheriting classes should implement these
 func press() -> void:
