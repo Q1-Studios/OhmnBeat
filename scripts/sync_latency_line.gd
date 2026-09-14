31b042dc5be7e04e6df7c21e1a999a0b2e760bd2
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-09-14 20:02:15 +0200

    Make latency calibration line pulse when matching with playback

--- scripts/sync_latency_line.gd
+++ scripts/sync_latency_line.gd
@@ -4,12 +4,19 @@ class_name LatencyLine
 @export var audio: AudioStreamPlayer
 @export var label: Label
 @export var start_screen_percentage: float = 0.5
+@export var pulse_color: Color = Color.DARK_RED
+@export var pulse_duration: float = 0.1
 
 @onready var y_distance: float = get_viewport_rect().size.y
+@onready var default_modulate: Color = modulate
 
 const SPEED: int = 40
 
 var legal_mouse_init: bool = false
+
+var last_playback_pos: float = 0
+var allow_pulse: bool = true
+var current_pulse_duration: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,6 +28,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if(not is_visible_in_tree()):
 		return
+	
+	# Allow pulse when loop has restarted
+	if last_playback_pos > audio.get_playback_position():
+		allow_pulse = true
+	
+	# Pulse after latency time has passed
+	if (allow_pulse and audio.get_playback_position() >= (get_latency() / 1000.0)):
+		current_pulse_duration = 0
+		modulate = pulse_color
+		allow_pulse = false
+	modulate = pulse_color.lerp(default_modulate, clamp(current_pulse_duration / pulse_duration, 0, 1))
+	current_pulse_duration += delta
+	
 	if(Input.is_action_pressed("ui_up")):
 		position.y -= SPEED * delta
 	if(Input.is_action_pressed("ui_down")):
@@ -28,6 +48,8 @@ func _process(delta: float) -> void:
 	if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and legal_mouse_init):
 		position.y = get_viewport().get_mouse_position().y
 	position.y = clamp(position.y, start_screen_percentage * y_distance, y_distance)
+	
+	last_playback_pos = audio.get_playback_position()
 
 
 func _input(event: InputEvent) -> void:
