b8044a3da7600f478b7aac695d94b781a87ef2af
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-09-14 21:07:33 +0200

    Improved menu start animation

--- scripts/show_when_all_hidden.gd
+++ scripts/show_when_all_hidden.gd
@@ -4,7 +4,7 @@ extends CanvasItem
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	for node in others:
 		if node.visible:
 			hide()
